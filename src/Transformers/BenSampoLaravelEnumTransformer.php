fe7dd8fbebc430a0a53ffd97e707dbdbedac1cad
Byron Adams <byron.adams54@gmail.com>
2020-12-31 23:00:28 +1300

    Adding support for transforming bensampo/laravel-enum enums

---
+++ src/Transformers/BenSampoLaravelEnumTransformer.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Spatie\LaravelTypeScriptTransformer\Transformers;
+
+use BenSampo\Enum\Enum;
+use ReflectionClass;
+use Spatie\TypeScriptTransformer\Structures\TransformedType;
+use Spatie\TypeScriptTransformer\Transformers\Transformer;
+
+class BenSampoLaravelEnumTransformer implements Transformer
+{
+    public function canTransform(ReflectionClass $class): bool
+    {
+        return $class->isSubclassOf(Enum::class);
+    }
+
+    public function transform(ReflectionClass $class, string $name): TransformedType
+    {
+        return TransformedType::create(
+            $class,
+            $name,
+            "export enum {$name} {" . PHP_EOL . $this->resolveOptions($class) . PHP_EOL . "}"
+        );
+    }
+
+    private function resolveOptions(ReflectionClass $class): string
+    {
+        /** @var Enum $enum */
+        $enum = $class->getName();
+
+        $options = array_map(
+            fn($key) => "  {$key} = ".json_encode($enum::getValue($key)).",",
+            $enum::getKeys()
+        );
+
+        return implode(PHP_EOL, $options);
+    }
+}
---
+++ tests/Transformers/BenSampoLaravelEnumTransformerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Spatie\LaravelTypeScriptTransformer\Tests\Transformers;
+
+use BenSampo\Enum\Enum;
+use ReflectionClass;
+use Spatie\LaravelTypeScriptTransformer\Tests\TestCase;
+use Spatie\LaravelTypeScriptTransformer\Transformers\BenSampoLaravelEnumTransformer;
+use Spatie\Snapshots\MatchesSnapshots;
+
+class BenSampoLaravelEnumTransformerTest extends TestCase
+{
+    use MatchesSnapshots;
+
+    private BenSampoLaravelEnumTransformer $transformer;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->transformer = new BenSampoLaravelEnumTransformer();
+    }
+
+    /** @test */
+    public function it_will_check_if_an_enum_can_be_transformed()
+    {
+        $enum = new class(10) extends Enum {
+            public const ADMIN = 10;
+            public const USER = 20;
+        };
+
+        $noEnum = new class {
+        };
+
+        $this->assertTrue($this->transformer->canTransform(new ReflectionClass($enum)));
+        $this->assertFalse($this->transformer->canTransform(new ReflectionClass($noEnum)));
+    }
+
+    /** @test */
+    public function it_can_transform_an_enum()
+    {
+        $enum = new class('foobar') extends Enum {
+            public const ADMIN = 10;
+            public const USER = 20;
+            public const STRING_USER = 'foobar';
+        };
+
+        $type = $this->transformer->transform(new ReflectionClass($enum), 'Enum');
+
+        $this->assertMatchesSnapshot($type->transformed);
+        $this->assertTrue($type->missingSymbols->isEmpty());
+    }
+
+
+}
