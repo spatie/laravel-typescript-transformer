d55addbf9f6611fce5fb11c9b9d3b029ebde6e50
rubenvanassche <rubenvanassche@users.noreply.github.com>
2026-03-13 13:41:04 +0000

    Fix styling

--- tests/FakeClasses/DataWithAttributes.php
+++ tests/FakeClasses/DataWithAttributes.php
@@ -8,38 +8,27 @@ use Spatie\LaravelData\Attributes\MapOutputName;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Lazy;
 use Spatie\LaravelData\Optional;
-use Spatie\LaravelTypeScriptTransformer\Tests\FakeClasses\CustomLazy;
 use Spatie\TypeScriptTransformer\Attributes\Hidden;
 
 class DataWithAttributes extends Data
 {
     public function __construct(
         public string $plain,
-
         #[Hidden]
         #[DataHidden]
         public string $bothHidden,
-
         #[Hidden]
         public string $onlyTsHidden,
-
         #[DataHidden]
         public string $onlyDataHidden,
-
         #[MapOutputName('mapped_output')]
         public string $withMapOutputName,
-
         #[MapName(input: 'input_name', output: 'output_name')]
         public string $withMapNameBothSet,
-
         public string|Lazy $lazyProperty,
-
         public string|Optional $optionalProperty,
-
         public string|int|Lazy|Optional $lazyAndOptionalProperty,
-
         public string|CustomLazy $customLazyProperty,
-
         public ?string $nullableProperty,
     ) {
     }
